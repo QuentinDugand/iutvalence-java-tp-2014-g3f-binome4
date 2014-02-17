1947f373c31162b6a4366f861a2ff3c3424289f8
dugandq <dugandq@PC-B102-04.iut-valence.fr>
2014-02-17 16:37:25 +0100

    represente une partie de Rush Hour

---
+++ src/LanceurDeRushHour.java
@@ -0,0 +1,10 @@
+
+public class LanceurDeRushHour {
+	public static void main (String[] argo){
+		System.out.println("Création d'une partie de RushHour");
+		RushHour partiedeRushHour = new RushHour();
+		System.out.println("Demarrage de la partie");
+		partiedeRushHour.jouer();
+		System.out.println("Fin de la partie");
+	}
+}
