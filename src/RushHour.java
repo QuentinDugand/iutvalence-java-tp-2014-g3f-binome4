4dff5bdfdfd868c2aaeb5b6c03374fb24bfad144
dugandq <dugandq@PC-B102-04.iut-valence.fr>
2014-02-17 17:28:49 +0100

    creation du debut du jeu

--- src/RushHour.java
+++ src/RushHour.java
@@ -1,4 +1,24 @@
-
+/**
+ * nouvelle partie de Rush Hour
+ */
 public class RushHour {
+	
+	private Grille grille;
+	/**
+	 * Contient les voitures
+	 */
+	private Joueur joueur;
+	
+	public RushHour(){
+		this.grille = new Grille();
+		this.joueur = new Joueur();
+		
+	}
+	/**
+	 * permet au jouer de lancer une partie de Rush Hour
+	 */
+	public void jouer(){
+		
+	}
 
 }
