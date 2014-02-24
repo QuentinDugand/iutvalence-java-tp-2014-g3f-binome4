cf1ff36cad9c4c99d8596ff59d11d872186c5e11
sebastienjean <baz.jean@gmail.com>
2014-02-24 14:09:26 +0100

    survol du code, ajout de remarques

--- src/RushHour.java
+++ src/RushHour.java
@@ -1,24 +1,33 @@
+// TODO déplacer la classe dans un paquetage
+
 /**
  * nouvelle partie de Rush Hour
  */
-public class RushHour {
-	
+public class RushHour
+{
+	// TODO écrire un commentaire
 	private Grille grille;
+	
+	// TODO réécrire un commentaire (incohérent)
 	/**
 	 * Contient les voitures
 	 */
 	private Joueur joueur;
-	
-	public RushHour(){
+
+	// TODO écrire un commentaire
+	public RushHour()
+	{
 		this.grille = new Grille();
 		this.joueur = new Joueur();
-		
 	}
+
+	// TODO écrire un commentaire plus précis
 	/**
 	 * permet au jouer de lancer une partie de Rush Hour
 	 */
-	public void jouer(){
-		
+	public void jouer()
+	{
+
 	}
 
 }
