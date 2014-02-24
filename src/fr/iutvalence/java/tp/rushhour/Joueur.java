c95544c1b21937bbbb2012363d28064db02d0de1
dugandq <dugandq@PC-B102-04.iut-valence.fr>
2014-02-24 17:31:02 +0100

    package et commentaire de joueur

--- src/fr/iutvalence/java/tp/rushhour/Joueur.java
+++ src/fr/iutvalence/java/tp/rushhour/Joueur.java
@@ -1,6 +1,13 @@
-// TODO déplacer la classe dans un paquetage
+// TODO déplacer la classe dans un paquetage(fait)
 package fr.iutvalence.java.tp.rushhour;
 // TODO écrire un commentaire
+/**
+ * Est caractérisé par un nom
+ * Déplace les voitures
+ * Entre un score
+ * @author dugandq
+ *
+ */
 public class Joueur 
 {
 
